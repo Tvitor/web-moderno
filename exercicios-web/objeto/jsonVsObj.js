2fb67d84ad0d47f48f21c8217db1f1f1e222261f
vitor <contato.vst@gmail.com>
2019-02-11 01:09:41 -0200

    comentarioJson

--- exercicios-web/objeto/jsonVsObj.js
+++ exercicios-web/objeto/jsonVsObj.js
@@ -1,7 +1,7 @@
-const obj = { a: 1, b: 2, c: 3, soma() { return a + b + c } }
-console.log(JSON.stringify(obj))
+const obj = 'https://cefis.com.br/api/v1/event'
+console.log(JSON.stringify(obj)) //migra o dado convertendo para um formato texto (json)
 
 // console.log(JSON.parse("{ a: 1, b: 2, c: 3 }"))
 // console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3 }"))
-console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 }'))
+console.log(JSON.parse('{ "data": [{}]'))
 console.log(JSON.parse('{ "a": 1.7, "b": "string", "c": true, "d": {}, "e": [] }'))
